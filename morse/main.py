3de7f6331f9f25ce48db588cefb540e30088ae6b
Amandus Søve Thorsrud <ama.thorsrud@gmail.com>
2021-01-18 15:45:33 +0100

    morse: initial working version

--- morse/main.py
+++ morse/main.py
@@ -64,18 +64,103 @@ class Debouncer:
 
 
 class MorseDecoder:
+    class Signal(Enum):
+        DOT = auto()
+        DASH = auto()
+        SHORT_PAUSE = auto()
+        MEDIUM_PAUSE = auto()
+        LONG_PAUSE = auto()
 
-    def __init__(self):
+    current_word = ''
+    current_symbol = ''
+    __last_rise = 0
+    __last_fall = 0
+
+    def __init__(self, interval, max_input_deviation, max_pause_deviation, too_long_is_dash):
         self.debouncer = Debouncer(PIN_BTN, GPIO.PUD_UP)
-        self.__max_deviance_ms = 100
+        self.interval = interval
+        self.max_input_deviation = max_input_deviation
+        self.max_pause_deviation = max_pause_deviation
+        self.too_long_is_dash = too_long_is_dash
+
+    def value_in_interval(self, value, max_deviation, interval_multiplier):
+        interval = self.interval * interval_multiplier
+        if (interval - max_deviation) <= value <= (interval + max_deviation):
+            return True
+        return False
+
+    def input_in_interval(self, value, interval_multiplier=1):
+        return self.value_in_interval(value, self.max_input_deviation, interval_multiplier)
+
+    def pause_in_interval(self, value, interval_multiplier=1):
+        return self.value_in_interval(value, self.max_pause_deviation, interval_multiplier)
+
+    def read_one_signal(self):
+        signal = None
+
+        if self.debouncer.rise():
+            self.__last_rise = time.monotonic_ns()
+            elapsed_ms = (self.__last_rise - self.__last_fall) / 1000000
+
+            if self.pause_in_interval(elapsed_ms, 3):
+                signal = self.Signal.MEDIUM_PAUSE
+                print('Pause duration: ' + str(elapsed_ms))
+        elif self.debouncer.fall():
+            self.__last_fall = time.monotonic_ns()
+            elapsed_ms = (self.__last_fall - self.__last_rise) / 1000000
+
+            if self.input_in_interval(elapsed_ms):
+                signal = self.Signal.DOT
+            elif self.input_in_interval(elapsed_ms, 3):
+                signal = self.Signal.DASH
+            elif self.too_long_is_dash and elapsed_ms > (self.interval * 3):
+                signal = self.Signal.DASH
+            else:
+                print('Unrecognized')
+
+            print('Elapsed: ' + str(elapsed_ms))
+        elif self.__last_fall > self.__last_rise:
+            elapsed_ms = (time.monotonic_ns() - self.__last_fall) / 1000000
+
+            if elapsed_ms >= (self.interval * 7 - self.max_pause_deviation):
+                signal = self.Signal.LONG_PAUSE
+                self.__last_rise = 0
+                self.__last_fall = 0
+
+        return signal
+
+    def process_signal(self, signal):
+        if signal in [self.Signal.DOT, self.Signal.DASH]:
+            self.update_current_symbol(signal)
+        elif signal is self.Signal.MEDIUM_PAUSE:
+            self.handle_symbol_end()
+        elif signal is self.Signal.LONG_PAUSE:
+            self.handle_word_end()
+
+    def update_current_symbol(self, signal):
+        if signal is self.Signal.DOT:
+            self.current_symbol += '.'
+        else:
+            self.current_symbol += '-'
+
+    def handle_symbol_end(self):
+        self.update_current_word(MORSE_CODE.get(self.current_symbol, '_'))
+        self.current_symbol = ''
+
+    def update_current_word(self, symbol):
+        self.current_word += symbol
+
+    def handle_word_end(self):
+        self.handle_symbol_end()
+        print(self.current_word)
+        self.current_word = ''
 
     def run_decoding_loop(self):
         while True:
             self.debouncer.update()
-            if self.debouncer.rise():
-                print('Started pressing!')
-            elif self.debouncer.fall():
-                print('Stopped pressing!')
+            signal = self.read_one_signal()
+            if signal is not None:
+                self.process_signal(signal)
 
 
 def main():
@@ -85,7 +170,26 @@ def main():
     GPIO.setup(PIN_RED_LED_2, GPIO.OUT, GPIO.LOW)
     GPIO.setup(PIN_BTN, GPIO.IN, GPIO.PUD_UP)
 
-    decoder = MorseDecoder()
+    interval = int(input('Choose a time interval for T (in ms): '))
+    input_deviation = int(
+        input('Choose a max deviation for each input (in ms): '))
+
+    pause_deviation = input(
+        'Choose a max deviation for each pause (in ms) [Press ENTER for same as input]: ')
+    if pause_deviation == "":
+        pause_deviation = input_deviation
+    else:
+        pause_deviation = int(pause_deviation)
+
+    too_long_is_dash = input(
+        'Should an input that is too long be recognized as a dash? [Y/n]: ')
+    if too_long_is_dash == '' or too_long_is_dash.lower() == 'y':
+        too_long_is_dash = True
+    else:
+        too_long_is_dash = False
+
+    decoder = MorseDecoder(interval, input_deviation,
+                           pause_deviation, too_long_is_dash)
     decoder.run_decoding_loop()
 
 
